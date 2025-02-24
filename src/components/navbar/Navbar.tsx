d771e0ff2ce26a0e828355f56a22cf377b4bdfb9
Wandsonslopes <wandson_lopes@hotmail.com>
2025-02-24 13:04:55 -0300

    Construção dos Componentes Home, Navbar e Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                <Link to='/home' className="text-2xl font-bold">Farmacia FW</Link>
+
+                    <div className='flex gap-4'>
+                        Produtos
+                        Categorias
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+
+function Home() {
+    return (
+        <>
+            <div className="bg-indigo-900 flex justify-center">
+                <div className='container grid grid-cols-2 text-white'>
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className='text-5xl font-bold'>
+                            Seja Bem Vindo!
+                        </h2>
+                        <p className='text-xl'>
+                            A saúde é o nosso maior remédio
+                        </p>
+
+                        <div className="flex justify-around gap-4">
+                            <div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+                                >
+                                Produtos
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center ">
+                        <img
+                            src="https://cdn.pixabay.com/photo/2020/08/03/09/39/medical-5459633_1280.png"
+                            alt="Imagem Página Home"
+                            className='w-2/3'
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
